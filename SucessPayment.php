0621384b84a92fa889efa8deae37294e41810b12
Anderson Nunes Souza <andersouza@paypal.com>
2022-06-19 01:43:08 -0300

    Updated Front-end

--- SucessPayment.php
+++ SucessPayment.php
@@ -1,21 +1,44 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" style="max-height: 800px;">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sucess Payment
-        <html>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-        </html>
-    </title>
+    <title>Sucess Payment</title>
 </head>
-
 <body>
-    <h1>Id da transação: <?php $paymenId = $_GET["paymentId"]; echo $paymenId; ?></h1>
-    <h2>Pagamento realizado com sucesso!</h2>
-    <h2><a href="./index.php">Retornar ao site da loja</h2></a>
+    <div class="container-flex">
+            <header class="text-center col-12 bg-secondary p-3">
+                PayPal Plus
+            </header>
+
+        <div class="col-12 bg-light text-center p-5">
+            <h1>Id da transação: <?php $paymenId = $_GET["paymentId"];
+                                    echo $paymenId; ?></h1>
+            <h2>Pagamento realizado com sucesso!</h2>
+            <div class="p-3">
+                <h2><a href="./index.php">Retornar ao site da loja</h2></a>
+            </div>
+
+             <div class="row p-5"></div>
+             <div class="row p-5"></div>
+             <div class="row p-5"></div>
+             <div class="row p-5"></div>
+             <div class="row p-5"></div>
+             <div class="row p-4"></div>
+        </div>
+        
+
+        <div class="col-12 p-3 bg-secondary">
+            
+            <footer class="text-center">
+                PayPal do Brasil
+            </footer>
+        </div>
+    </div>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -5,98 +5,95 @@
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
      <script src="https://www.paypalobjects.com/webstatic/ppplusdcc/ppplusdcc.min.js" type="text/javascript"></script>
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
      <title>Plus Project</title>
  </head>
 
- <div class="card card-body bg-secondary h-100">
+ <body>
 
-     <body>
-         <header>
+     <div class="container-flex">
+
+         <header class="text-center col-12 bg-secondary p-3">
+             PayPal Plus
          </header>
 
-         <div id="ppplusDiv"></div>
+         <div class="col-12 bg-light p-5">
+             <div id="ppplusDiv"></div>
 
+             <div class="d-grid col-6 mx-auto p-3">
+                 <button type="submit" id="continueButton" class="btn btn-lg btn-primary" onclick="ppp.doContinue(); return false;">
+                     Checkout
+                 </button>
+             </div>
 
-         <div class="d-grid gap-2 col-6 mx-auto h-100">
-             <button type="submit" id="continueButton" class="btn btn-lg btn-primary" onclick="ppp.doContinue(); return false;">
-                 Checkout
-             </button>
+             <div class="row p-5"></div>
+             <div class="row p-5"></div>
+             <div class="row p-4"></div>
          </div>
-         <div class="card-footer">
-             <footer class="mt-auto text-white-50 text-center">
-                 PayPal do Brasil
+
+         <div class="col-12 p-3 bg-secondary">
+             <footer class="text-center">
+                 PayPal do Brasil © 1999 - <script>
+                     document.write(new Date().getFullYear());
+                 </script>
              </footer>
          </div>
-         <script type="application/javascript">
-             var url = <?php print_r(strval(require_once('./phps/CreatePayment.php'))); ?>;
-             //console.table(url);
-             var rememberedCards = "customerRememberedCardHash";
-             var installments = null;
 
-             var ppp = PAYPAL.apps.PPP({
-                 "approvalUrl": url.links[1].href,
-                 "placeholder": "ppplusDiv",
-                 "mode": "sandbox",
-                 "payerFirstName": "Anderson",
-                 "payerLastName": "Souza",
-                 "payerPhone": " 5511979730795",
-                 "payerEmail": "teste@outlook.com",
-                 "payerTaxId": "38384480818",
-                 "payerTaxIdType": "BR_CPF",
-                 "language": "pt_BR",
-                 "country": "BR",
-                 "rememberedCards": rememberedCards,
-                 "enableContinue": "continueButton",
-                 "merchantInstallmentSelectionOptional": installments,
-                 "onContinue": () => {
-                     $.ajax({
-                         url: "./phps/paymentExecution.php",
-                         type: "POST",
-                         data: {
-                             field1: payerId, // payerid vai aqui,
-                             field2: url.links[2].href
-                         },
-                         success: function(result) {
-                             //console.log(typeof(result)); //->String
-                             console.log(result);
-                             //console.table(result);
-                             result = JSON.parse(result);
-                             //console.log(typeof(result)); //->Object
-                             console.table(result);
-                             //console.log(result.id);
-                             //console.table(result.id);
-                             //console.table(result.transactions);
-                             //console.table(result.transactions[0].related_resources[0].sale.id);
-                             alert("Pagamento Concluído");
-                             window.location.href = "./SucessPayment.php?paymentId=" + result.transactions[0].related_resources[0].sale.id;
-                         },
-                         error: function() {
-                             //console.log(error);
-                             alert("function Error");
-                             window.location.href = "./CancelPayment.html"
-                         }
-                     })
-                 },
-             });
+     </div>
 
-             window.addEventListener("message", messageListener, false);
+     <script type="application/javascript">
+         var url = <?php print_r(strval(require_once('./phps/CreatePayment.php'))); ?>;
+         var rememberedCards = "customerRememberedCardHash";
 
-             function messageListener(event) {
-                 var data = JSON.parse(event.data);
-                 //console.table(data);
-                 if (data.action == "checkout") {
-                     payerId = data.result.payer.payer_info.payer_id;
-                     //console.log(data.result.payer.payer_info.payer_id);
-                 } else {}
-             };
-         </script>
-         <br>
+         var ppp = PAYPAL.apps.PPP({
+             "approvalUrl": url.links[1].href,
+             "placeholder": "ppplusDiv",
+             "mode": "sandbox",
+             "payerFirstName": "John",
+             "payerLastName": "Doe",
+             "payerPhone": "5511954854582",
+             "payerEmail": "johndoe@email.com",
+             "payerTaxId": "19850755806",
+             "payerTaxIdType": "BR_CPF",
+             "language": "pt_BR",
+             "country": "BR",
+             "rememberedCards": rememberedCards,
+             "enableContinue": "continueButton",
+             "iframeHeight": "450",
+             "onContinue": () => {
+                 $.ajax({
+                     url: "./phps/paymentExecution.php",
+                     type: "POST",
+                     data: {
+                         field1: payerId,
+                         field2: url.links[2].href
+                     },
+                     success: function(result) {
+                         result = JSON.parse(result);
+                         console.table(result);
+                         alert("Pagamento Concluído");
+                         window.location.href = "./SucessPayment.php?paymentId=" + result.transactions[0].related_resources[0].sale.id;
+                     },
+                     error: function() {
+                         window.location.href = "./CancelPayment.html"
+                     }
+                 })
+             },
+         });
 
- </div>
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+         window.addEventListener("message", messageListener, false);
+
+         function messageListener(event) {
+             var data = JSON.parse(event.data);
+             if (data.action == "checkout") {
+                 payerId = data.result.payer.payer_info.payer_id;
+             } else {}
+         };
+     </script>
+
  </body>
 
  </html>
